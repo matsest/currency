de395ef25ee37dc67ee281f9455b43a4c39da767
Mats Estensen <mats.est@gmail.com>
2016-01-28 11:45:16 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import urllib2
 import json
@@ -6,6 +6,7 @@ import json
 raw = urllib2.urlopen("http://www.norges-bank.no/api/Currencies/USD?language=no").read()
 json_object = json.loads(raw)
 
+today_date = json_object["Daily"]["Values"][0]["DateString"]
 f = open('USDNOK_%s.csv' % today_date.replace(".", "") ,'w')
 
 for elem in json_object["Daily"]["Values"]:
